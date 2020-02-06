80c91ddea11bfa86df505f8a30105d655576c951
irinkov <iris.kov88@gmail.com>
2020-02-06 21:55:33 +0200

    Add Our Team

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-    $('.button[filter]').click(function() {
-        if($(this).attr('filter')=='all') {
-            if($(this).attr('val')=='off') {
+$(document).ready(function () {
+    $('.button[filter]').click(function () {
+        if ($(this).attr('filter') == 'all') {
+            if ($(this).attr('val') == 'off') {
                 $('.button[filter]').attr('vall', 'off');
                 $(this).attr('val', 'on');
                 $('.button[filter]').removeClass('focused');
@@ -9,14 +9,25 @@ $(document).ready(function() {
                 $('.filter > div').show(300);
             }
         } else
-        if($(this).attr('val')=='off') {
-            $('.button[filter]').attr('val', 'off');
-            $(this).attr('val', 'on');
-            $('.button[filter]').removeClass('focused');
-            $(this).addClass('focused');
-            $('.filter > div').hide(300);
-            var filter = $(this).attr('filter');
-            $('.filter > div[filter='+filter+']').show(300);
-        }
+            if ($(this).attr('val') == 'off') {
+                $('.button[filter]').attr('val', 'off');
+                $(this).attr('val', 'on');
+                $('.button[filter]').removeClass('focused');
+                $(this).addClass('focused');
+                $('.filter > div').hide(300);
+                var filter = $(this).attr('filter');
+                $('.filter > div[filter=' + filter + ']').show(300);
+            }
     })
+    // Slick Slider
+
+    $('.multiple-items').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows: false,
+        dots: true,
+        dotsClass: 'dots-style'
+    });
+
 })
